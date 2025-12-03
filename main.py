f98ed42c6f523c4fec32f1f368fa34d9f33dac14
Harsh Sharma <hs210310222360@gmail.com>
2025-12-02 18:12:18 -0700

    checkpoint schema

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
 
 @app.get("/")
 async def read_root():
     return {"Hello": "new World"}
 
 @app.post("/createpost")
-async def create_post(payload : dict = Body(...)):
-    print(payload)
-    return {"Post created": "title: " + payload['title'] + "  content:  " + payload['content']}
+async def create_post(new_post: Post):
+    print(new_post.published)
+    return {"Data": "new_post"}
