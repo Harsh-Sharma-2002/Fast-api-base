3ec7de0257fce197312ab1be168fdd8a786ad73a
Harsh Sharma <hs210310222360@gmail.com>
2025-12-02 18:48:10 -0700

    checkpoint_3 schema

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
+from random import randrange
 
 app = FastAPI()
 
-my_posts = [{"title": "title of post 1", "content": "content of post 1","published": True, "rating": 5, "id": 1},
-            {"title": "title of post 2", "content": "content of post 2", "published": False, "rating": 4, "id": 2}]
+my_posts = [{"title": "title of post 1", "content": "content of post 1","published": True, "rating": 5, "id": randrange(0,1000000)},
+            {"title": "title of post 2", "content": "content of post 2", "published": False, "rating": 4, "id": randrange(0,1000000)}]
 
 class Post(BaseModel):
     title: str
@@ -27,6 +28,6 @@ async def get_post():
 async def create_post(new_post: Post):
     print(new_post.dict())
     dict_temp = new_post.dict()
-    dict_temp['id'] = len(my_posts) + 1
+    dict_temp['id'] = randrange(0, 1000000)
     my_posts.append(dict_temp)
     return {"Data": "new_post"}
