046153b97a28d9b08063c7eac9ea26ce8e9f3d34
Harsh Sharma <hs210310222360@gmail.com>
2025-12-04 16:38:30 -0700

    defined db structure with sqlalchemy

--- app/main.py
+++ app/main.py
@@ -7,6 +7,9 @@ from utils import find_post, find_index_post
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 app = FastAPI()
 
@@ -28,6 +31,11 @@ class UpdatePost(BaseModel):
     rating: Optional[int] = None
 
 ###################################################################
+
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:new_password@localhost/fastapi'
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
 
 while(True):
     try:
