ee8a92767694f534ca2d4271bc345e1ffd24f438
Harsh Sharma <hs210310222360@gmail.com>
2025-12-12 14:55:00 -0500

    updated main.py and models.py to use orm and created new pydantic objects for different functions

--- app/models.py
+++ app/models.py
@@ -1,12 +1,46 @@
 from .database import Base
 from sqlalchemy import Column, Integer, String, Boolean
 from sqlalchemy.sql.sqltypes import TIMESTAMP
+from sqlalchemy.sql.expression import text
+from pydantic import BaseModel
+from typing import Optional
+from datetime import datetime
+
 
 class PostModel(Base):
     __tablename__ = "posts"
 
-    id = Column(Integer, primary_key=True, index=True,nullable=False)
+    id = Column(Integer, primary_key=True, index=True, nullable=False)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
-    published = Column(Boolean, server_default = 'True')
-    created_at = Column(TIMESTAMP(timezone=True), nullable=False,server_default="now()")
+
+    published = Column(Boolean, server_default=text("TRUE"), nullable=False)
+    created_at = Column(
+        TIMESTAMP(timezone=True), nullable=False, server_default=text("now()")
+    )
+
+
+class CreatePost(BaseModel):
+    title: str
+    content: str
+    published: Optional[bool] = True
+
+class ResponsePost(BaseModel):
+    id: int
+    title: str
+    content: str
+    published: bool
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
+
+     
+
+class UpdatePost(BaseModel):
+    title: Optional[str] = None
+    content: Optional[str] = None
+    published: Optional[bool] = True
+ 
+  
+
